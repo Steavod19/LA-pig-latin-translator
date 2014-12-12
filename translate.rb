d92a9c067fa652b217770060e765ba29aa747991
Steve Rogers <srogers.design@gmail.com>
2014-12-12 17:45:54 -0500

    deleted trash files

--- translate.rb
+++ translate.rb
@@ -1,4 +1,3 @@
-require 'pry'
 def translate(sentence)
   final_answer = []
   vs = ["a", "e", "i", "o", "u"]
@@ -6,14 +5,14 @@ def translate(sentence)
   sentence.each do |word|
     first_letter = word[0]
 
-  if vs.include?(first_letter)
-     final_word = word + "way"
-     final_answer.push(final_word)
-   else
-    	first_vowel = word.index(/[aeiouy]/)
-    	arr = word.partition(word[first_vowel])
-    	final = "#{arr[1]}#{arr[2]}#{arr[0]}ay"
-    	final_answer.push(final)
+    if vs.include?(first_letter)
+      final_word = word + "way"
+      final_answer.push(final_word)
+    else
+      first_vowel = word.index(/[aeiouy]/)
+      arr = word.partition(word[first_vowel])
+      final = "#{arr[1]}#{arr[2]}#{arr[0]}ay"
+      final_answer.push(final)
     end
   end
   final_answer.join(" ")
